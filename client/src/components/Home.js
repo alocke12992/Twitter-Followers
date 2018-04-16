e05bc38c5f2886fb1f4c86f7a3b950941037933b
Andrew Locke <alocke12992@gmail.com>
2018-04-16 12:15:02 -0600

    dbl click to remove followers

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react';
-import { Header, Image } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {Header, Image} from 'semantic-ui-react';
 import axios from 'axios';
-import {StaggeredMotion, spring, presets} from 'react-motion'
+import {
+  Motion,
+  StaggeredMotion,
+  spring,
+  presets,
+} from 'react-motion';
+import Moved from './Moved';
 
 class Home extends Component {
+  state = {x: 250, y: 300, followers: [], moved: []}
 
-  state = {x: 250, y: 300, followers: []}
-
-  componentDidMount(){
+  componentDidMount() {
     window.addEventListener('mousemove', this.handleMouseMove)
+    window.addEventListener('dblclick', this.dropFollower)
     axios.get('/api/followers')
       .then(({data}) => this.setState({followers: data}))
+  }
+
+  dropFollower = () => {
+    const {moved, x, y} = this.state;
+    const [drop, ...followers] = this.state.followers;
+    this.setState({
+      followers,
+      moved: [...moved, {x, y, transitioned: false, ...drop}]
+    });
+  }
+
+  addFollower = (id) => {
+    const {moved, followers} = this.state;
+    const follower = moved.find(f => f.id === id)
+    this.setState({
+      followers: [...followers, follower],
+      moved: moved.filter(m => m.id !== id)
+    })
   }
 
   handleMouseMove = ({pageX: x, pageY: y}) => {
@@ -19,81 +43,93 @@ class Home extends Component {
 
   getStyles = (prevStyles) => {
     const endValue = prevStyles.map((_, i) => {
-      return i === 0 
-      ? this.state
-      : 
-      {
-         x: spring(prevStyles[i -1].x, presets.gentle),
-         y: spring(prevStyles[i -1].y, presets.gentle)
-      }
+      return i === 0
+        ? this.state
+        : {
+          x: spring(prevStyles[i - 1].x, presets.gentle),
+          y: spring(prevStyles[i - 1].y, presets.gentle),
+        }
     })
     return endValue
   }
 
-  render() {
-    const {followers} = this.state
-    if (followers.length){
-      return(
-        <StaggeredMotion
-          defaultStyles={
-            followers.map( () => ({x: 0, y: 0}))
-          }
-          styles={this.getStyles}
-        >
-          { avatars =>  
-            <div style={styles.container}>
-              {avatars.map(({x, y}, i) => {
-                  let follower = followers[i]
-                  if (follower) {
-                    return(
-                      <div 
-                        key={i} 
-                        style={
-                        {borderRadius: '99px', 
-                        backgroundColor: 'white',
-                        width: '150px',
-                        height: '150px',
-                        border: '3px solid white', 
-                        position: 'absolute',
-                        backgroundSize: '150px',
-                        backgroundImage: `url(${follower.img}`,
-                        webkitTransform: `translate3d(${x - 50}px, ${y - 50}px, 0`,
-                        transform: `translate3d(${x - 50}px, ${y - 50}px, 0`,
-                        zIndex: avatars.length -i,
-                        }
-                      }
-                      >
-                      </div>
-                    )
-                  }
-                })
-              }
-            </div>
-          }
-        </StaggeredMotion>
+  movedFollowers = () => {
+    const {moved} = this.state;
+    const moveable = moved.filter(m => !m.transitioned)
+    return moveable.map(follower => {
+      return (
+        <Moved
+          {...follower}
+          key={follower.id}
+          addFollower={this.addFollower}
+        />
       )
-    } else{
-      return null 
-    }
+    })
   }
+
+  render() {
+    return (
+      <div>
+        {this.movedFollowers()}
+        {this.state.followers.length > 0 &&
+          <StaggeredMotion
+            defaultStyles={this.state.followers.map(() => ({x: 0, y: 0}))}
+            styles={this.getStyles}>
+            {avatars =>
+              <div style={{width: '100%', height: '100%', position: 'absolute', background: 'black'}}>
+                {avatars.map(({x, y}, i) => {
+                  let follower = this.state.followers[i];
+                  if (follower) {
+                    return (
+                      <div
+                        key={i}
+                        style={{
+                          borderRadius: '99px',
+                          backgroundColor: 'white',
+                          width: '50px',
+                          height: '50px',
+                          border: '3px solid white',
+                          position: 'absolute',
+                          backgroundSize: '50px',
+                          backgroundImage: `url(${this.state.followers[i].img})`,
+                          WebkitTransform: `translate3d(${x - 50}px, ${y - 50}px, 0)`,
+                          transform: `translate3d(${x - 50}px, ${y - 50}px, 0)`,
+                          zIndex: avatars.length - i,
+                        }}
+                      />
+                    )
+                  } else {
+                    return null
+                  }
+                }
+                )}
+              </div>
+            }
+          </StaggeredMotion>
+        }
+      </div>
+    );
+  };
+
 }
 
 const styles = {
   container: {
     width: '100%',
     height: '100%',
+    position: 'absolute',
     background: 'black',
-    position: 'absolute'
   },
   person: {
-    borderRadius: '99px', 
+    borderRadius: '99px',
     backgroundColor: 'white',
     width: '50px',
     height: '50px',
-    border: '3px solid white', 
+    border: '3px solid white',
     position: 'absolute',
     backgroundSize: '50px',
   }
+
 }
 
 export default Home;
---
+++ client/src/components/StyledAvatar.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  border-radius: 99px;
+  background-color: white;
+  width: 75px;
+  height: 75px;
+  border: 3px solid white;
+  position: z absolute;
+  background-size: 75px;
+`
